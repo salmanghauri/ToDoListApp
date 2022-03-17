90d6fe731f736ac99959f320dd7898667fd50f13
Salman Ghauri <salmanghauri@hotmail.com>
2022-03-17 17:34:07 -0400

    styling changes

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {body} = require('express-validator');
 
 const authCtrl = require('../controllers/auth');
+const isLoggedIn = require('../helper/isLoggedin');
 const multer = require('multer');
 
 let storage = multer.diskStorage({
@@ -17,7 +18,7 @@ var upload = multer({ storage: storage })
 
 router.get("/signup", authCtrl.auth_signup_get);
 router.get("/signin", authCtrl.auth_signin_get);
-router.get('/profile', authCtrl.auth_profile_get);
+router.get('/profile', isLoggedIn, authCtrl.auth_profile_get);
 router.post("/signup", [
     body('firstName').isLength({min : 2}).withMessage("first name must be at least 2 characters long"),
     body('lastName').isLength({min : 2}),
@@ -25,6 +26,6 @@ router.post("/signup", [
     body('password').isLength({min : 5}),
 ], upload.single('image'), authCtrl.auth_signup_post);
 router.post("/signin", authCtrl.auth_signin_post);
-router.get("/logout", authCtrl.auth_logout_get);
+router.get("/logout", isLoggedIn, authCtrl.auth_logout_get);
 
 module.exports = router;
